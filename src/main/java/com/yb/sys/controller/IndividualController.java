9f176969d7b185c0e6ef12a354d89854b663b406
root <root@fdr20x64.localdomain>
2014-10-07 12:14:08 +0800

    individual:show blank if there is no value for a field

--- src/main/java/com/yb/sys/controller/IndividualController.java
+++ src/main/java/com/yb/sys/controller/IndividualController.java
@@ -33,8 +33,6 @@ import com.yb.sys.entity.SchoolExt;
 import com.yb.sys.model.SchoolModel;
 import com.yb.sys.service.ISchoolServiceExt;
 
-import com.yb.sys.entity.LanguageExt;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -87,12 +85,6 @@ public class IndividualController {
 	public String goView(@ModelAttribute IndividualModel individualModel, ModelMap model){
 		if(individualModel.getDataId() != 0){
 			IndividualExt individualExt = individualService.load(individualModel.getDataId(), true);
-      
-      System.out.println("Yuanguo: languages");
-      for(LanguageExt lang: individualExt.getlanguages())
-      {
-        System.out.println("Yuanguo:"+lang.getId()+", "+lang.getlanguage());
-      }
 			individualModel.setIndividualExt(individualExt);
 		}
 		
